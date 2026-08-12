d80531aa373e675f7bc06277bca832597fe3340c
Thiago Rocha <thiagorocha841@gmail.com>
2026-08-12 12:03:14 -0300

    change: reemplazo fecha

--- src/components/Faltan.jsx
+++ src/components/Faltan.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 export default function Faltan() {
   const calcularTiempoRestante = () => {
-    const diferencia = new Date("2026-10-17") - new Date();
+    const diferencia = new Date("2026-10-23 21:00:00") - new Date();
 
     let tiempoRestante = {
       dias: 0,
@@ -42,9 +42,15 @@ export default function Faltan() {
         pt-10 pb-10 md:pt-16 md:pb-16 tracking-[0.2em] 
     "
     >
-      <h2 className="md:text-xl lg:text-2xl" data-aos="fade-up">FALTAN</h2>
+      <h2 className="md:text-xl lg:text-2xl" data-aos="fade-up">
+        FALTAN
+      </h2>
       {tiempo ? (
-        <div className="flex text-[2.5rem] md:text-[3.5rem] lg:text-[4rem] gap-1 font-normal" data-aos="zoom-in" data-aos-delay="200">
+        <div
+          className="flex text-[2.5rem] md:text-[3.5rem] lg:text-[4rem] gap-1 font-normal"
+          data-aos="zoom-in"
+          data-aos-delay="200"
+        >
           <p>{tiempo.dias.toString().padStart(2, "0")}</p>
           <p>:</p>
           <p>{tiempo.horas.toString().padStart(2, "0")}</p>
@@ -55,7 +61,13 @@ export default function Faltan() {
         </div>
       ) : null}
 
-      <h2 className="pt-1 md:text-xl lg:text-2xl" data-aos="fade-up" data-aos-delay="400">PARA CELEBRAR</h2>
+      <h2
+        className="pt-1 md:text-xl lg:text-2xl"
+        data-aos="fade-up"
+        data-aos-delay="400"
+      >
+        PARA CELEBRAR
+      </h2>
     </section>
   );
 }
